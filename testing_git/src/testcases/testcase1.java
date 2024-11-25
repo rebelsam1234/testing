f1759d06b7e69310e40b1db670a7a35e64c61221
rebelsam1234 <rajsam1201@gmail.com>
2024-11-25 12:52:23 +0530

    first commit...

---
+++ testing_git/src/testcases/testcase1.java
@@ -0,0 +1,15 @@
+package testcases;
+
+import java.util.Scanner;
+
+public class testcase1 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first string: ");
+        String input = scanner.nextLine();
+     
+		
+	}
+}
